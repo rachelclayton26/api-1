536a786546f1b43eaaf0479081a682a3f7d7f1f7
Rachel Christopher <rachelchristopher@Rachels-MBP.localdomain>
2021-03-25 22:18:13 -0500

    3/25 v4

--- script.js
+++ script.js
@@ -71,6 +71,64 @@ fetchPokemons();
 
 /* WORKING PROGRESS FOR PORTFOLIO */ 
 
+// document.getElementById("name").addEventListener('submit', event =>{
+// 	if (event.target.value == "") {
+//         document.getElementsByClassName("poke_container").innerHTML("<h1> Please enter valid Pokemon <h1>");
+//     } else {
+//         const getPokemon2 = async id => {
+// 			const singleURL = `https://pokeapi.co/api/v2/pokemon/${event.target.value}`;
+// 			const single = await fetch(singleURL);
+// 			const pokemon2 = await single.json();
+// 			createPokemonCard2(pokemon2);
+// 		};
+		
+// 		function createPokemonCard2(pokemon2) {
+// 			const pokemonEl2 = document.createElement('div');
+// 			pokemonEl.classList.add('pokemon2');
+		
+// 			const poke_types2 = pokemon.types.map(type => type.type.name);
+// 			const type2 = main_types.find(type => poke_types.indexOf(type) > -1);
+// 			const name2 = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+// 			const color2 = colors[type];
+			
+// 			pokemonEl2.style.backgroundColor = color;
+		
+// 			const pokeInnerHTML2 = `
+// 				<div class="title">
+// 					<div class="name">${name}</div>
+// 					<div class="number">#${pokemon.id
+// 											.toString()
+// 											.padStart(3, '0')}</div>
+// 				</div>
+				
+// 				<div class="img-container">
+// 					<img src="https://pokeres.bastionbot.org/images/pokemon/${
+// 									pokemon.id
+// 								}.png" alt="${name}" />
+// 				</div>
+				
+// 				<div class="info"> 
+// 					<small class="type">Type: <span>${type}</span></small>
+// 				</div>	
+// 			`;
+		
+// 			pokemonEl.innerHTML = pokeInnerHTML2;
+		
+// 			poke_container.appendChild(pokemonEl2);
+// 		}
+//     }
+	// console.log(event.target.value);
+
+	// const getPokeName = async name => {
+	// 	const url2 = `https://pokeapi.co/api/v2/pokemon/${name}`;
+	// 	const res2 = await fetch(url2);
+	// 	const pokemon2 = await res2.json();
+	// 	createPokemonCard(pokemon2);
+	// 	};
+				
+	// 	fetchPokeByName(getPokeName);
+	// })
+
 // const nameSearch = document.querySelector('#name');
 // const numberSearch = document.querySelector('#number');
 // const typeSearch = document.querySelector('#type');
